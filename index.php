06c7f2f72b34160516600768e45af2479f388a55
jgharris1 <98436606+jgharris1@users.noreply.github.com>
2022-02-10 21:40:51 -0600

    Add files via upload
    updated js and user page working towards timeline post system

--- index.php
+++ index.php
@@ -15,52 +15,51 @@
 		</div>
 		<div id="content" class="row">
 			<div id="stream">
-            	<?php
-			
-                	/*$dbc = new mysqli("localhost", "root", "23pass", "mydb");
-                    	$query = "select * from post_exp";
-                    	$result = $dbc->query($query);
-                     	while($row = $result->fetch_assoc())*///for when i manage to work out an sql server for now an xml server as an example
-			$xml=simplexml_load_file("postexp.xml");//in the meantime using an xml will work just changes some syntax
-			foreach($xml->post as $posts)
-                     	{
-                     		print 
-                      		"
-                          		<div id=\"exppost\">
-                              		<div class=\"post_content\">
-                      		";
-                      		print $posts->content_text;
-                      		print
-                      		"
-                                	</div>
-                          		<div class=\"post_action_bar\">
-                              		<div class=\"poster_id\">
-                      		";
-                      		print $posts->username;
-                      		print
-                      		"
-                                </div>
-                          	<div class=\"post_like\">likes
-                      		";
-                      		print $posts->likes;
-                      		print
-                      		"
-                                       	</div>
-                          		<div class=\"post_comment\">comments
-                      		";
-                      		print $posts->comment;
-                      		print
-                      		"
-                                        </div>
-                                  	<div class=\"post_share\">share</div>
-                                  	<div class=\"post_edit\">edit</div>
-                              		</div>
-                          		</div>
-					<br>
-                      		";
-                	}
-                ?>
-				<div id="newpost"><textarea id="text_input" maxlength="256" cols="64" name="text_input" rows="4">Say something interesting</textarea><br><div id="counter">25/256</div><input type="submit" /></div>
+            			<?php
+                			/*$dbc = new mysqli("localhost", "root", "23pass", "mydb");
+                    			$query = "select * from post_exp";
+                    			$result = $dbc->query($query);
+                     			while($row = $result->fetch_assoc())*///for when i manage to work out an sql server for now an xml server as an example
+					$xml=simplexml_load_file("postexp.xml");//in the meantime using an xml will work just changes some syntax
+					foreach($xml->post as $posts)
+                     			{
+                     				print 
+                      				"
+                          				<div id=\"exppost\">
+                              				<div class=\"post_content\">
+                      				";
+                      				print $posts->content_text;
+                      				print
+                      				"
+                                			</div>
+                          				<div class=\"post_action_bar\">
+                              				<div class=\"poster_id\">
+                      				";
+                      				print $posts->username;
+                      				print
+                      				"
+                                			</div>
+                          				<div class=\"post_like\">likes
+                      				";
+                      				print $posts->likes;
+                      				print
+                      				"
+                                		       	</div>
+                          				<div class=\"post_comment\">comments
+                      				";
+                      				print $posts->comment;
+                      				print
+                      				"
+                                		        </div>
+                                		  	<div class=\"post_share\">share</div>
+                                		  	<div class=\"post_edit\">edit</div>
+                              				</div>
+                          				</div>
+							<br>
+                      				";
+                			}
+                		?>
+					<div id="newpost"><textarea id="text_input" maxlength="256" cols="64" name="text_input" rows="4">Say something interesting</textarea><br><div id="counter">25/256</div><input id="text_submit" type="submit" name="submit" /></div>
 			</div>
 			<div id="friends">friend links(this gets loaded from a .js)</div>
 		</div>
--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 window.addEventListener("load", link_events, false);
 
+var xhr = false; 
+
 function link_events()
 {
-	document.getElementById("text_input").onkeydown = HelloWorld;
-	document.getElementById("text_input").onclick = HelloWorld;
+	document.getElementById("text_input").onkeydown = charcount;
+	document.getElementById("text_input").onclick = charcount;
+	document.getElementById("text_submit").onclick = newpost;
+	xhr = new XMLHttpRequest();
+	if(xhr)
+	{
+		xhr.open("GET", "postexp.xml", true);
+		xhr.send(null);	
+	}
 }
 
-function HelloWorld()
+function charcount()
 {
 	var str = document.getElementById("text_input").value;
 	var location = document.getElementById("counter");
 	var len = str.length;
 	var newstr = len + "/256";
 	location.innerHTML = newstr;
+}
+
+function newpost()
+{
+	var location = document.getElementById("sitedat");
+    	var oldxml = xhr.responseText;
+	var newxml = old
+	
+	//var str = document.getElementById("text_input").value;
 }
