99942266bbdbd79dbb49e5f17b4199ad98e86e12
Noah Williams <noahwilliams212@gmail.com>
2022-04-08 21:51:18 -0500

    Added a class to test_views.py

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,1 +1,15 @@
-from django.test import SimpleTestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from userpage.models import User, Post, Comment, Like, Friendlink
+import json
+
+
+class TestViews(TestCase):
+
+    def test_newacc(self):
+        client = Client()
+
+        response = client.get(reverse('list'))
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'userpage/')
